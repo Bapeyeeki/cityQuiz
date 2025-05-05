8458ff6269fba6aa4f9600fdb4a3a12b7e483df9
Szymon Choruży <szymonchoruzy@iMac-schoruzy.localdomain>
2025-05-05 15:54:20 +0200

    add database connection

---
+++ public/getCity.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: application/json');
+require_once 'db.php';
+
+$database = new Database();
+$conn = $database->getConnection();
+
+$cityName = isset($_GET['city']) ? trim($_GET['city']) : null;
+if ($cityName) {
+    $stmt = $conn->prepare("SELECT nazwa, populacja, szerokosc_geograficzna, dlugosc_geograficzna FROM miasta WHERE LOWER(nazwa) = LOWER(:city) LIMIT 1");
+    $stmt->bindParam(':city', $cityName);
+    $stmt->execute();
+
+    $city = $stmt->fetch(PDO::FETCH_ASSOC);
+    echo $city ? json_encode($city) : json_encode(['error' => 'Miasto nie znalezione.']);
+} else {
+    echo json_encode(['error' => 'Brak parametru city']);
+}
+?>
