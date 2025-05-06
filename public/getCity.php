6e51aae073800ddd66610eb36e6d038aff16cec6
DawidLD <dawidek.l@interia.pl>
2025-05-06 12:52:58 +0200

    test

--- public/getCity.php
+++ public/getCity.php
@@ -7,10 +7,7 @@ $conn = $database->getConnection();
 
 $cityName = isset($_GET['city']) ? trim($_GET['city']) : null;
 if ($cityName) {
-<<<<<<< HEAD
     // Zmiana zapytania SQL pod nową tabelę i kolumny
-=======
->>>>>>> 2bef3bb033dacc3bdc077c3206a47df0a51149b7
     $stmt = $conn->prepare("SELECT city, lat, lng, populatio FROM cities WHERE LOWER(city) = LOWER(:city) LIMIT 1");
     $stmt->bindParam(':city', $cityName);
     $stmt->execute();
